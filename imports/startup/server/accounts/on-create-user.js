7ad0bee6f6edbb4c237452be23331f4aebc546d0
Dave <dhurls99@gmail.com>
2018-11-17 14:11:31 -0500

    add attributes on login

--- imports/startup/server/accounts/on-create-user.js
+++ imports/startup/server/accounts/on-create-user.js
@@ -1,8 +1,13 @@
 import { Accounts } from 'meteor/accounts-base';
-import sendWelcomeEmail from '../../../api/Users/server/send-welcome-email';
 
 Accounts.onCreateUser((options, user) => {
   const userToCreate = user;
   if (options.profile) userToCreate.profile = options.profile;
+  userToCreate.profile.points = 0;
+  userToCreate.profile.games_played = 0;
+  userToCreate.profile.euchres_recieved = 0;
+  userToCreate.profile.euchres_given = 0;
+  userToCreate.profile.in_game = false;
   return userToCreate;
 });
+
